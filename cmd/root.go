804bc06ad2c9df5b74477b6b77200ed72b5c9fd3
mindulle <mindullestudio@gmail.com>
2024-01-10 02:04:28 +0900

    :rotating_light: Check error when to call a command with help option

--- cmd/root.go
+++ cmd/root.go
@@ -4,15 +4,17 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"log"
 	"os"
 
+	"github.com/hoehwa/gopkg/git"
+	"github.com/mindulle/mdexec/internal"
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands.
-var rootCmd = &cobra.Command{
+var RootCmd = &cobra.Command{
 	Use:   "mdexec",
-	Short: "A brief description of your application",
+	Short: "Executable utilites for better developer expirience",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
@@ -21,13 +23,21 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		if _, err := os.ReadDir(internal.BaseDir); err != nil {
+			git.Clone(internal.Owner, internal.Repository, internal.BaseDir)
+		}
+		err := cmd.Help()
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
@@ -39,8 +49,9 @@ func init() {
 	// will be global for your application.
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mdexec.yaml)")
+	RootCmd.PersistentFlags().BoolP("raw", "r", false, "Print raw source code of the current command.")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
